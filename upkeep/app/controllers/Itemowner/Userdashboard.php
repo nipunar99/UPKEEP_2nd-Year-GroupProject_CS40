71f58b68d1a55d42bdc75a33cf455df0421691ff
pramodkavi <pramodkavindu44@gmail.com>
2022-11-26 15:02:31 +0530

    AddItem routing

--- upkeep/app/controllers/Itemowner/Userdashboard.php
+++ upkeep/app/controllers/Itemowner/Userdashboard.php
@@ -4,13 +4,8 @@ class Userdashboard {
 
     use Controller;
     public function index (){
-<<<<<<< HEAD
         
         if($_SESSION['USER'] == $_SESSION['id']){
-=======
-         
-        if($_SESSION['USER'] == 'Owner'){
->>>>>>> 4e7d436571894f0f062259a5443fd187a7280e1d
             $this->view('Itemowner/userDashboard');
         }else{
             redirect("Home/home");
@@ -19,4 +14,3 @@ class Userdashboard {
     }
 
 }
- 
